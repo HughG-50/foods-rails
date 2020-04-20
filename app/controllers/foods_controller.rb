544b0dc5c6626847e91b06e67f73c21daee1a97e
HughG-50 <hugh.greethead@gmail.com>
2020-04-20 17:39:35 +1000

    Create new food form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -9,14 +9,21 @@ class FoodsController < ApplicationController
     end
 
     def create 
-
+        new_food = Food.create(name: params["name"], calories: params["calories"].to_i)
+        redirect_to foods_path
     end
 
     def update 
-
+        updated_food = Food.update(params["id"], name: params["name"], calories: params["calories"])
+        redirect_to food_path(params["id"])
     end
 
     def destroy 
+        destroyed_food = Food.find(params["id"]).destroy 
+        redirect_to foods_path
+    end
+
+    def new 
 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
-  get "/foods", to: "foods#index"
-  get "/foods/:id", to: "foods#show"
+  get "/foods", to: "foods#index", as: "foods"
+  # We need a page to put the form on to create a new food
+  # Remember that this line needs to be above the dynamic route line
+  get "/foods/new", to: "foods#new", as: "new_food"
   
-  post "/foods/:id", to: "foods#create"
+  # Show single food
+  get "/foods/:id", to: "foods#show", as: "food"
+  post "/foods", to: "foods#create"
+
+  
   put "/foods/:id", to: "foods#update"
 
   delete "/foods/:id", to: "foods#destroy"
