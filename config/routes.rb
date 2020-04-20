b1ac106d0bd1f30e637c6e5f48b3743eb712e78b
HughG-50 <hugh.greethead@gmail.com>
2020-04-20 14:53:17 +1000

    Updated routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/foods", to: "foods#index"
+  get "/foods/:id", to: "foods#show"
+  
+  post "/foods/:id", to: "foods#create"
+  put "/foods/:id", to: "foods#update"
+
+  delete "/foods/:id", to: "foods#destroy"
+
 end
